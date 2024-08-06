45507d17051906f9b484d8a8160f61042adf67a4
sasikumarkeskaran <78546975+sasikumarkeskaran@users.noreply.github.com>
2024-08-06 14:13:35 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -12,7 +12,7 @@ public class Misaligned {
     }
     public static void main(String[] args) { 
         int result = printColorMap();
-        assert(result == 24);
+        assert(result == 25);
         System.out.println("All is well (maybe!)");
     }
 }
