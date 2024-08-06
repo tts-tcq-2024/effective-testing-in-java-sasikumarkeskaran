0841dc1e896c180c35adcce8a3cfb114584a4a32
sasikumarkeskaran <78546975+sasikumarkeskaran@users.noreply.github.com>
2024-08-06 12:31:39 +0530

    Update alerter.java

--- alerter/alerter.java
+++ alerter/alerter.java
@@ -19,7 +19,7 @@ public class Alerter {
         }
     }
     public static void main(String[] args) {
-        alertInCelcius(400.5f);
+        alertInCelcius(0);
         alertInCelcius(303.6f);
         System.out.printf("%d alerts failed.\n", alertFailureCount);
         System.out.println("All is well (maybe!)\n");
